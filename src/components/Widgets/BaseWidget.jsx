97103f6e72904ead68ed8d156f30006bb75984ac
kkps-d <freakykaung@gmail.com>
2023-11-22 12:37:23 -0800

    Starting over

--- src/components/Widgets/BaseWidget.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./BaseWidget.module.css";
-
-function BaseWidget({ className = "", children }) {
-  return <div className={`${styles.widget} ${className}`}>{children}</div>;
-}
-
-export default BaseWidget;
