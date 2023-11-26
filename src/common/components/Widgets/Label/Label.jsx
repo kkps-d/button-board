4022cc20b5779827b65cb86f9e1afda070601205
kkps-d <freakykaung@gmail.com>
2023-11-25 21:30:55 -0800

    Added label component

--- src/common/components/Widgets/Label/Label.jsx
+++ src/common/components/Widgets/Label/Label.jsx
@@ -1,7 +1,60 @@
 import styles from "./Label.module.css";
+import * as Descriptions from "../Descriptions.js";
+import { useEffect, useLayoutEffect, useRef } from "react";
+import fitToContainer from "../../../fitToContainer.js";
 
+/**
+ * Label widget
+ * @param {Object} props
+ * @param {Descriptions.Label} props.description
+ * @returns
+ */
 function Label({ description }) {
-  return <div className={`widget ${styles.label}`}>Label</div>;
+  const ref = useRef(null);
+
+  const {
+    label = "A label",
+    fontSize = "fit",
+    align = "center",
+    showBorders = true,
+  } = description.state;
+
+  let alignItems;
+  switch (align) {
+    case "left":
+      alignItems = "start";
+      break;
+    case "right":
+      alignItems = "end";
+      break;
+    case "center":
+      alignItems = "center";
+      break;
+  }
+
+  useEffect(() => {
+    if (fontSize == "fit") {
+      document.fonts.ready.then(() => fitToContainer(ref.current));
+    } else if (fontSize.endsWith("px") || fontSize.endsWith("pt")) {
+      ref.current.style.fontSize = fontSize;
+    }
+  }, [label, fontSize]);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        textAlign: align,
+        alignItems: alignItems,
+        borderColor: `${
+          showBorders ? "var(--color-widget-border)" : "transparent"
+        }`,
+      }}
+      className={`widget ${styles.label}`}
+    >
+      {label}
+    </div>
+  );
 }
 
 export default Label;
