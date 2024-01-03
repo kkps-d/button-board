e441994c7e4a51d46587e6a9d0d3fe1051c1a500
kkps-d <freakykaung@gmail.com>
2024-01-02 19:10:17 -0800

    major repo restructure to accomodate multi page client code and server code

---
+++ web/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, "app", "index.html"),
+        configure: resolve(__dirname, "configure", "index.html"),
+      },
+    },
+  },
+});
